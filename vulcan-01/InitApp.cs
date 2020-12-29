43bef9860c5edabc885992f72f0f81982bc34588
xuri02 <t0bl4s.durst@gmail.com>
2020-12-29 18:38:13 +0100

    moved some stuff to init

---
+++ vulcan-01/InitApp.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SharpVk;
+using SharpVk.Glfw;
+using SharpVk.Khronos;
+using Buffer = SharpVk.Buffer;
+
+namespace vulcan_01
+{
+    internal partial class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var pg = new Program();
+            pg.Run();
+            await Task.Delay(1000);
+            Environment.Exit(0);
+        }
+        
+        private void Run()
+        {
+            InitWindow();
+            InitVulkan();
+            MainLoop();
+            Cleanup();
+        }
+
+        private void InitWindow()
+        {
+            Glfw3.Init();
+
+            Glfw3.WindowHint(WindowAttribute.ClientApi, 0);
+            window = Glfw3.CreateWindow(800, 500, "First test", MonitorHandle.Zero, WindowHandle.Zero);
+            Glfw3.SetWindowSizeCallback(window, RecreateSwapChain);
+
+            glfw3.Glfw3.Public.SetWindowSizeLimits_0(window.RawHandle, 100,100, 10000,10000);
+        }
+
+        private void InitVulkan()
+        {
+            CreateInstance();
+            CreateSurface();
+            PickPhysicalDevice();
+            CreateLogicalDevice();
+            CreateSwapChain();
+            CreateImageViews();
+            CreateRenderPass();
+            CreateShaderModules();
+            CreateGraphicsPipeline();
+            CreateFrameBuffers();
+            CreateCommandPools();
+            CreateBufferManager();
+            bufferManager.AddBuffer(vertices, indices);
+            CreateCommandBuffers();
+            CreateSemaphores();
+        }
+
+        private void MainLoop()
+        {
+            while (!Glfw3.WindowShouldClose(window))
+            {
+                DrawFrame();
+
+                Glfw3.PollEvents();
+            }
+        }
+
+        private void DrawFrame()
+        {
+            var nextImage = swapChain.AcquireNextImage(uint.MaxValue, imageAvailableSemaphore, null);
+
+            graphicsQueue.Submit(
+                new SubmitInfo
+                {
+                    CommandBuffers = new[]
+                    {
+                        commandBuffers[nextImage]
+                    },
+                    SignalSemaphores = new[]
+                    {
+                        renderFinishedSemaphore
+                    },
+                    WaitDestinationStageMask = new[]
+                    {
+                        PipelineStageFlags.ColorAttachmentOutput
+                    },
+                    WaitSemaphores = new[]
+                    {
+                        imageAvailableSemaphore
+                    }
+                },
+                null);
+
+            presentQueue.Present(renderFinishedSemaphore, swapChain, nextImage, new Result[1]);
+        }
+    }
+}
--- vulcan-01/Program.cs
+++ vulcan-01/Program.cs
@@ -8,34 +8,6 @@ namespace vulcan_01
 {
     internal partial class Program
     {
-
-
-        private static async Task Main(string[] args)
-        {
-            var pg = new Program();
-            pg.Run();
-            await Task.Delay(1000);
-            Environment.Exit(0);
-        }
-
-        private void Run()
-        {
-            InitWindow();
-            InitVulkan();
-            MainLoop();
-            Cleanup();
-        }
-
-        private void InitWindow()
-        {
-            Glfw3.Init();
-
-            Glfw3.WindowHint(WindowAttribute.ClientApi, 0);
-            window = Glfw3.CreateWindow(800, 500, "First test", MonitorHandle.Zero, WindowHandle.Zero);
-
-            Glfw3.SetWindowSizeCallback(window, RecreateSwapChain);
-        }
-
         private readonly Vertex[] vertices =
         {
             new(new(-0.5f, -0.5f), new(1.0f, 0.0f, 0.0f)), new(new(0.5f, -0.5f), new(0.0f, 1.0f, 0.0f)), new(new(0.5f, 0.5f), new(0.0f, 0.0f, 1.0f)), new(new(-0.5f, 0.5f), new(1.0f, 1.0f, 1.0f))
@@ -45,62 +17,5 @@ namespace vulcan_01
         {
             0, 1, 2, 2, 3, 0
         };
-        private void InitVulkan()
-        {
-            CreateInstance();
-            CreateSurface();
-            PickPhysicalDevice();
-            CreateLogicalDevice();
-            CreateSwapChain();
-            CreateImageViews();
-            CreateRenderPass();
-            CreateShaderModules();
-            CreateGraphicsPipeline();
-            CreateFrameBuffers();
-            CreateCommandPools();
-            CreateVertexBuffers(vertices);
-            CreateIndexBuffer(indices);
-            CreateCommandBuffers(indices);
-            CreateSemaphores();
-        }
-
-        private void MainLoop()
-        {
-            while (!Glfw3.WindowShouldClose(window))
-            {
-                DrawFrame();
-
-                Glfw3.PollEvents();
-            }
-        }
-
-        private void DrawFrame()
-        {
-            var nextImage = swapChain.AcquireNextImage(uint.MaxValue, imageAvailableSemaphore, null);
-
-            graphicsQueue.Submit(
-                new SubmitInfo
-                {
-                    CommandBuffers = new[]
-                    {
-                        commandBuffers[nextImage]
-                    },
-                    SignalSemaphores = new[]
-                    {
-                        renderFinishedSemaphore
-                    },
-                    WaitDestinationStageMask = new[]
-                    {
-                        PipelineStageFlags.ColorAttachmentOutput
-                    },
-                    WaitSemaphores = new[]
-                    {
-                        imageAvailableSemaphore
-                    }
-                },
-                null);
-
-            presentQueue.Present(renderFinishedSemaphore, swapChain, nextImage, new Result[1]);
-        }
     }
 }
