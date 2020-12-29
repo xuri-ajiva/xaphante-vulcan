f14444516785d5b645a9cf7d56efef9fa3ab2c51
xuri02 <t0bl4s.durst@gmail.com>
2020-12-29 16:41:33 +0100

    added programm for vulcan

---
+++ vulcan-01/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Threading.Tasks;
+using SharpVk;
+using SharpVk.Glfw;
+using SharpVk.Khronos;
+
+namespace vulcan_01
+{
+    internal partial class Program
+    {
+
+
+        private static async Task Main(string[] args)
+        {
+            var pg = new Program();
+            pg.Run();
+            await Task.Delay(1000);
+            Environment.Exit(0);
+        }
+
+        private void Run()
+        {
+            InitWindow();
+            InitVulkan();
+            MainLoop();
+            Cleanup();
+        }
+
+        private void InitWindow()
+        {
+            Glfw3.Init();
+
+            Glfw3.WindowHint(WindowAttribute.ClientApi, 0);
+            window = Glfw3.CreateWindow(800, 500, "First test", MonitorHandle.Zero, WindowHandle.Zero);
+
+            Glfw3.SetWindowSizeCallback(window, RecreateSwapChain);
+        }
+
+        private readonly Vertex[] vertices =
+        {
+            new(new(-0.5f, -0.5f), new(1.0f, 0.0f, 0.0f)), new(new(0.5f, -0.5f), new(0.0f, 1.0f, 0.0f)), new(new(0.5f, 0.5f), new(0.0f, 0.0f, 1.0f)), new(new(-0.5f, 0.5f), new(1.0f, 1.0f, 1.0f))
+        };
+
+        private readonly ushort[] indices =
+        {
+            0, 1, 2, 2, 3, 0
+        };
+        private void InitVulkan()
+        {
+            CreateInstance();
+            CreateSurface();
+            PickPhysicalDevice();
+            CreateLogicalDevice();
+            CreateSwapChain();
+            CreateImageViews();
+            CreateRenderPass();
+            CreateShaderModules();
+            CreateGraphicsPipeline();
+            CreateFrameBuffers();
+            CreateCommandPools();
+            CreateVertexBuffers(vertices);
+            CreateIndexBuffer(indices);
+            CreateCommandBuffers(indices);
+            CreateSemaphores();
+        }
+
+        private void MainLoop()
+        {
+            while (!Glfw3.WindowShouldClose(window))
+            {
+                DrawFrame();
+
+                Glfw3.PollEvents();
+            }
+        }
+
+        private void DrawFrame()
+        {
+            var nextImage = swapChain.AcquireNextImage(uint.MaxValue, imageAvailableSemaphore, null);
+
+            graphicsQueue.Submit(
+                new SubmitInfo
+                {
+                    CommandBuffers = new[]
+                    {
+                        commandBuffers[nextImage]
+                    },
+                    SignalSemaphores = new[]
+                    {
+                        renderFinishedSemaphore
+                    },
+                    WaitDestinationStageMask = new[]
+                    {
+                        PipelineStageFlags.ColorAttachmentOutput
+                    },
+                    WaitSemaphores = new[]
+                    {
+                        imageAvailableSemaphore
+                    }
+                },
+                null);
+
+            presentQueue.Present(renderFinishedSemaphore, swapChain, nextImage, new Result[1]);
+        }
+
+        private void RecreateSwapChain(WindowHandle window, int width, int height)
+        {
+            device.WaitIdle();
+
+            commandPool.FreeCommandBuffers(commandBuffers);
+
+            foreach (var frameBuffer in frameBuffers)
+            {
+                frameBuffer.Dispose();
+            }
+            frameBuffers = null;
+
+            pipeline.Dispose();
+            pipeline = null;
+
+            pipelineLayout.Dispose();
+            pipelineLayout = null;
+
+            foreach (var imageView in swapChainImageViews)
+            {
+                imageView.Dispose();
+            }
+            swapChainImageViews = null;
+
+            renderPass.Dispose();
+            renderPass = null;
+
+            swapChain.Dispose();
+            swapChain = null;
+
+            CreateSwapChain();
+            CreateImageViews();
+            CreateRenderPass();
+            CreateGraphicsPipeline();
+            CreateFrameBuffers();
+            CreateCommandBuffers(indices);
+        }
+    }
+}
