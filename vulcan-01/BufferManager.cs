95ce2514e55c34017d0e247d82f6802b55511a8d
xuri02 <t0bl4s.durst@gmail.com>
2020-12-29 18:38:33 +0100

    added buffer manager

---
+++ vulcan-01/BufferManager.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using SharpVk;
+using Buffer = SharpVk.Buffer;
+
+namespace vulcan_01
+{
+    internal record BufferPack(Buffer VertexBuffer, DeviceMemory VertexBufferMemory, uint VerticesLength, Buffer IndexBuffer, DeviceMemory IndexBufferMemory, uint IndicesLength);
+
+    internal class BufferManager : IDisposable
+    {
+        private Device device;
+        private PhysicalDevice physicalDevice;
+        private Queue transferQueue;
+        private CommandPool transientCommandPool;
+
+        public object BufferLock { get; } = new();
+        public List<BufferPack> Buffers { get; } = new();
+
+        public BufferManager(Device device, PhysicalDevice physicalDevice, Queue transferQueue, CommandPool transientCommandPool)
+        {
+            this.device = device;
+            this.physicalDevice = physicalDevice;
+            this.transferQueue = transferQueue;
+            this.transientCommandPool = transientCommandPool;
+        }
+
+        public void AddBuffer(Vertex[] vertices, ushort[] indices)
+        {
+            lock (BufferLock)
+            {
+                var (vertexBuffer, vertexBufferMemory) = CreateVertexBuffers(vertices);
+                var (indexBuffers, indexBufferMemory) = CreateIndexBuffer(indices);
+                Buffers.Add(new(vertexBuffer, vertexBufferMemory, (uint)vertices.Length, indexBuffers, indexBufferMemory, (uint)indices.Length));
+            }
+        }
+
+        public (Buffer vertexBuffer, DeviceMemory vertexBufferMemory) CreateVertexBuffers(Vertex[] vertices)
+        {
+            var vertexSize = Unsafe.SizeOf<Vertex>();
+
+            var bufferSize = (uint)(vertexSize * vertices.Length);
+
+            CreateBuffer(bufferSize, BufferUsageFlags.TransferSource, MemoryPropertyFlags.HostVisible | MemoryPropertyFlags.HostCoherent, out var stagingBuffer, out var stagingBufferMemory);
+
+            var memoryBuffer = stagingBufferMemory.Map(0, bufferSize, MemoryMapFlags.None);
+
+            for (var index = 0; index < vertices.Length; index++)
+            {
+                Marshal.StructureToPtr(vertices[index], memoryBuffer + (vertexSize * index), false);
+            }
+
+            stagingBufferMemory.Unmap();
+
+            CreateBuffer(bufferSize, BufferUsageFlags.TransferDestination | BufferUsageFlags.VertexBuffer, MemoryPropertyFlags.DeviceLocal, out var vertexBuffer, out var vertexBufferMemory);
+
+            CopyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+            stagingBuffer.Dispose();
+            stagingBufferMemory.Free();
+
+            return (vertexBuffer, vertexBufferMemory);
+        }
+
+        public (Buffer indexBuffers, DeviceMemory indexBufferMemory) CreateIndexBuffer(ushort[] indices)
+        {
+            var indexSize = Unsafe.SizeOf<ushort>();
+
+            ulong bufferSize = (uint)(indexSize * indices.Length);
+
+            CreateBuffer(bufferSize, BufferUsageFlags.TransferSource, MemoryPropertyFlags.HostVisible | MemoryPropertyFlags.HostCoherent, out var stagingBuffer, out var stagingBufferMemory);
+
+            var memoryBuffer = stagingBufferMemory.Map(0, bufferSize, MemoryMapFlags.None);
+
+            for (var index = 0; index < indices.Length; index++)
+            {
+                Marshal.StructureToPtr(indices[index], memoryBuffer + (indexSize * index), false);
+            }
+
+            stagingBufferMemory.Unmap();
+
+            CreateBuffer(bufferSize, BufferUsageFlags.TransferDestination | BufferUsageFlags.IndexBuffer, MemoryPropertyFlags.DeviceLocal, out var indexBuffers, out var indexBufferMemory);
+
+            CopyBuffer(stagingBuffer, indexBuffers, bufferSize);
+
+            stagingBuffer.Dispose();
+            stagingBufferMemory.Free();
+
+            return (indexBuffers, indexBufferMemory);
+        }
+
+        public void CreateBuffer(ulong size, BufferUsageFlags usage, MemoryPropertyFlags properties, out Buffer buffer, out DeviceMemory bufferMemory)
+        {
+            buffer = device.CreateBuffer(size, usage, SharingMode.Exclusive, null);
+
+            var memRequirements = buffer.GetMemoryRequirements();
+
+            bufferMemory = device.AllocateMemory(memRequirements.Size, FindMemoryType(memRequirements.MemoryTypeBits, properties));
+
+            buffer.BindMemory(bufferMemory, 0);
+        }
+
+        public void CopyBuffer(Buffer sourceBuffer, Buffer destinationBuffer, ulong size)
+        {
+            var transferBuffers = device.AllocateCommandBuffers(transientCommandPool, CommandBufferLevel.Primary, 1);
+
+            transferBuffers[0].Begin(CommandBufferUsageFlags.OneTimeSubmit);
+
+            transferBuffers[0].CopyBuffer(sourceBuffer, destinationBuffer, new BufferCopy
+            {
+                Size = size
+            });
+
+            transferBuffers[0].End();
+
+            transferQueue.Submit(new SubmitInfo
+            {
+                CommandBuffers = transferBuffers
+            }, null);
+            transferQueue.WaitIdle();
+
+            transientCommandPool.FreeCommandBuffers(transferBuffers);
+        }
+
+        private uint FindMemoryType(uint typeFilter, MemoryPropertyFlags flags)
+        {
+            var memoryProperties = physicalDevice.GetMemoryProperties();
+
+            for (var i = 0; i < memoryProperties.MemoryTypes.Length; i++)
+            {
+                if ((typeFilter & (1u << i)) > 0
+                    && memoryProperties.MemoryTypes[i].PropertyFlags.HasFlag(flags))
+                {
+                    return (uint)i;
+                }
+            }
+
+            throw new("No compatible memory type.");
+        }
+
+        private void ReleaseUnmanagedResources()
+        {
+            device = null;
+            physicalDevice = null;
+            transferQueue = null;
+            transientCommandPool = null;
+        }
+
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~BufferManager()
+        {
+            ReleaseUnmanagedResources();
+        }
+
+        public void BindAllAndDraw(CommandBuffer commandBuffer)
+        {
+            lock (BufferLock)
+            {
+                foreach (var (vertexBuffer, _, _, indexBuffer, _, indicesLength) in Buffers)
+                {
+                    commandBuffer.BindVertexBuffers(0, vertexBuffer, 0);
+
+                    commandBuffer.BindIndexBuffer(indexBuffer, 0, IndexType.Uint16);
+                    commandBuffer.DrawIndexed(indicesLength, 1, 0, 0, 0);
+                }
+            }
+        }
+    }
+}
