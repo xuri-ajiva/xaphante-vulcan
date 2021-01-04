27d99955b84058df60274e5f26deec37e02b9240
xuri02 <t0bl4s.durst@gmail.com>
2021-01-04 17:39:54 +0100

    added test project and fixed TargetFramework

---
+++ WorkTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
